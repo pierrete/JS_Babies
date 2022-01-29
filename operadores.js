9244204c3d4ca20747a504ecaf8140fd1f843936
pierrete <akahime2014@gmail.com>
2022-01-29 13:41:39 -0300

    00

---
+++ operadores.js
@@ -0,0 +1,52 @@
+let lol = 17;
+console.log(lol)
+
+//código pra testar a Exponenciação
+let a = 2 ** 7
+console.log(a)
+
+let b = 15 / 2
+console.log(b)
+
+//testando o resto de uma divisão
+let c = 15 % 2
+console.log(c)
+
+//Adicionar ou diminuir 1
+console.log(lol++, lol--)
+
+//Soma os valores na primeira das duas variáveis / ou soma strings
+c += b
+console.log("Valor de c é "+ c + "  " + "Valor de b é  " + b)
+
+let d = "Meu pastel "
+let e = "é mais barato."
+console.log(d += e)
+
+//subtrai o valor e coloca na primeira variável
+console.log("O valor de b é ", b -= c)
+
+
+//multiplica e coloca na 1 var
+let f = 2;
+let g = 4;
+console.log(f *= g)
+console.log(f)
+console.log(g)
+
+//divide e coloca na 1 var
+console.log(g /= f)
+console.log(g)
+console.log(f)
+
+//exponencia e coloca na 1 var
+f = 2
+g = 3
+console.log(f **= g)
+console.log("valor de f ",f,"valor de g", g)
+
+
+let x = true
+let z = false
+// == os dois sã
+console.log(x == z)
